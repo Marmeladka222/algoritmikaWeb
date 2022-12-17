9c3b43fabc302ff2920f700a6c81344beec5244f
Marmeladka222 <user.email>
2022-12-17 13:25:16 +0300

    initial commit

---
+++ int = 5.py
@@ -0,0 +1,20 @@
+agrs = input('На список команд')
+
+print(int, str, float, bool)
+
+int = 5
+str = "5"
+float = 5.1
+bool = true
+
+while
+	if agrs == int:
+		print(True)
+	if agrs == str:
+		print(True)
+	if agrs == float:
+		print(True)
+	if agrs == bool:
+		print(True)
+	else:
+		print(False)
